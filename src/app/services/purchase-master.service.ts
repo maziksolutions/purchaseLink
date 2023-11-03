6c0505a911115f7148010ee0d8f6594f428f624f
Think Centre <Think Centre@DESKTOP-9VQDHTH>
2023-11-03 14:25:27 +0530

    Commit03.11.2023.01

--- src/app/services/purchase-master.service.ts
+++ src/app/services/purchase-master.service.ts
@@ -38,6 +38,24 @@ archiveServiceCategory(jobType: any[]): Observable < string > {
 }  
 //#endregion
 
+//Get Maintenance Type data
+getOrderTypes(status): Observable<any> {  
+  return this.httpClient.get<any[]>(`${this.linkurl}filterOrderType/${status}`, httpOptions);
+}
+getOrderTypeById(id):Observable<any>{  
+  return  this.httpClient.get<any>(this.linkurl + 'getOrderTypeById/' + id,httpOptions)
+  .pipe(catchError(this.handleError));
+}
+// Add New unit
+addOrderType(formData): Observable<any> {
+  return this.httpClient.post<any>(this.linkurl + 'addOrderType', formData)
+  .pipe(catchError(this.handleError));
+}
+archiveOrderType(jobType: any[]): Observable < string > {   
+  return this.httpClient.post<string> (`${this.linkurl}archiveOrderType/`, jobType, httpOptions);  
+}  
+//#endregion
+
 private handleError(error: HttpErrorResponse) {
   if (error.error instanceof ErrorEvent) {
     console.error('An error occurred:', error.error.message);
