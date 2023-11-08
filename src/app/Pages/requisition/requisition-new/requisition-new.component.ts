a0061ec43636a3c041a66e1606ad6266d28c9b19
Rahul Kumar <Rahul Kumar@DESKTOP-123DDPN>
2023-11-08 17:38:16 +0530

    chnages_08.11.2023.03

--- src/app/Pages/requisition/requisition-new/requisition-new.component.ts
+++ src/app/Pages/requisition/requisition-new/requisition-new.component.ts
@@ -1,5 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PurchaseMasterService } from '../../../services/purchase-master.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ImportDataComponent } from '../../common/import-data/import-data.component';
+import {
+  SelectionModel
+} from '@angular/cdk/collections';
+import { ViewChild } from '@angular/core';
+import { ExportExcelService } from 'src/app/services/export-excel.service';
+import { SwalToastService } from 'src/app/services/swal-toast.service';
+import { UserManagementService } from 'src/app/services/user-management.service';
+import { Router ,ActivatedRoute } from '@angular/router';
+import { RightsModel } from '../../Models/page-rights';
+import { registerNavEnum, unitMasterNavEnum } from '../../Shared/rights-enum';
+declare var $: any;
+declare let Swal, PerfectScrollbar: any;
 
 @Component({
   selector: 'app-requisition-new',
@@ -7,16 +26,56 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./requisition-new.component.css']
 })
 export class RequisitionNewComponent implements OnInit {
-  purchaseroute:any;
-  purchaseroutee:boolean=false;
+
+  orderForm: FormGroup; flag; pkey: number = 0;
+  displayedColumns: string[] = ['checkbox', 'orderTypes', 'defaultOrderType','serviceType','abbreviation'];
+  dataSource = new MatTableDataSource<any>();
+  selection = new SelectionModel<any>(true, []);
+  rights: RightsModel;
+  @ViewChild('searchInput') searchInput: ElementRef;
+  deletetooltip: any;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  selectedIndex: any;
+  projectnameAndcode:any;
+  orderTypes:any;
+  Priority:any;
+
   constructor(    private route: ActivatedRoute,
-    private router:Router,) { }
+    private router:Router, private purchaseService: PurchaseMasterService) { }
 
   ngOnInit(): void 
   {
+    this.LoadOrdertype();
+    this.LoadProjectnameAndcode();
+    this.LoadPriority();
    
   }
 
   
+  LoadOrdertype() {
+    this.purchaseService.getOrderTypes(0)
+      .subscribe(response => {
+        this.orderTypes = response.data;
+      })
+  }
+
+  LoadProjectnameAndcode() {
+    this.purchaseService.getprojectname(0)
+      .subscribe(response => {
+        this.projectnameAndcode = response.data;
+
+      })
+  }
+
+  LoadPriority() {
+    this.purchaseService.GetPreferenceType(0)
+      .subscribe(response => {
+        this.Priority = response.data;
+        console.log(this.Priority)
+      })
+  }
+
+
 
 }
